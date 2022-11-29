578ca5b0154675044243829a225da5e486f45d2c
Adarsh Kumar Gond <88982465+adarshkm5555@users.noreply.github.com>
2022-11-29 15:49:56 +0530

    removed my contract address
    Add your own contract address

--- utils/constant.js
+++ utils/constant.js
@@ -1,6 +1,6 @@
 import abi from './Lottery.json'
 
-export const contractAddress = "0x1D551cF565663A0F616C6De1604bC941f0E72251";
+export const contractAddress = "Your Deployed Contract Address";
 
 export const contractABI  = abi.abi;
 
